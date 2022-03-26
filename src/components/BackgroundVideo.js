7abbc541144bd4be0609533c457624080246e990
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2022-03-26 10:06:42 -0300

    Background video optimization

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,16 +1,39 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import styles from "../styles/components/BackgroundVideo.module.css";
 
 export function BackgroundVideo() {
+  const [pageLoad, setPageLoad] = useState(false);
+
+  useEffect(() => {
+    setPageLoad(true);
+  }, []);
+
   return (
     <>
       <div className={styles.overlay} />
       <video
         className={styles.background}
         autoPlay
+        playsInline
         muted
         loop
-        src="https://github.com/Animesports/animesports/blob/main/public/videos/soccer_otz.mp4?raw=true"
-      />
+        poster="/poster.webp"
+      >
+        {pageLoad && (
+          <>
+            <source
+              src="https://github.com/Animesports/animesports/blob/main/public/videos/soccer_web.webm?raw=true"
+              type="video/webm"
+            ></source>
+
+            <source
+              src="https://github.com/Animesports/animesports/blob/main/public/videos/soccer_otz.mp4?raw=true"
+              type="video/mp4"
+            ></source>
+          </>
+        )}
+      </video>
     </>
   );
 }
