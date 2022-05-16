f00fc1d65643bb8694e20a1300f2cf8d775585a8
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2022-05-16 17:08:29 -0300

    Soccer game update modal

--- src/components/SoccerEditor.js
+++ src/components/SoccerEditor.js
@@ -4,6 +4,9 @@ import { updateSoccerGame } from "../services/admin";
 import { getGameState, getSoccerGameById } from "../utils/Soccer";
 import { Loading } from "./Loading";
 import { SoccerScheduler } from "./SoccerScheduler";
+import { Input } from "./Input";
+import { Form } from "@unform/web";
+import styles from "../styles/components/SoccerEditor.module.css";
 
 export function SoccerEditor({ gameId, close }) {
   const { games, fetching } = useContext(soccerContext);
@@ -34,7 +37,79 @@ export function SoccerEditor({ gameId, close }) {
   );
 }
 
+import cStyles from "../styles/components/SoccerScheduler.module.css";
+import { low } from "../utils/Global";
+import { ISOtimeFormat, localDate } from "../utils/Date";
+
 function SoccerUpdater(game) {
   // Atualizar o placar, pode cancelar, pode encerrar
-  return <p>Atualizar Jogo</p>;
+  const team1 = game.teams.visited;
+  const team2 = game.teams.visitor;
+
+  const time = ISOtimeFormat(game.date);
+  const date = localDate(game.date);
+
+  function handleSubmit() {}
+
+  return (
+    <div className={[cStyles.container, styles.container].join(" ")}>
+      <Form className={cStyles.content} onSubmit={handleSubmit}>
+        <div className={cStyles.previewBox}>
+          <div className={cStyles.previewContent}>
+            <div className={cStyles.title}>
+              <strong>
+                Começou {time} • {date}
+              </strong>
+            </div>
+            <div className={cStyles.teamsBox}>
+              <div>
+                <img
+                  src={team1.logo ?? "/icons/soccer-shield.svg"}
+                  alt={team1.id}
+                />
+                <strong>{low(team1.name) ?? "Time"}</strong>
+
+                <Input
+                  name="visited"
+                  placeholder="0"
+                  defaultValue={game.score.visited}
+                  min={0}
+                  autoComplete="off"
+                  type="number"
+                />
+              </div>
+
+              <span className={cStyles.separator}>X</span>
+
+              <div>
+                <img
+                  src={team2.logo ?? "/icons/soccer-shield.svg"}
+                  alt={team2.id}
+                />
+                <strong>{low(team2.name) ?? "Time"}</strong>
+
+                <Input
+                  name="visitor"
+                  placeholder="0"
+                  defaultValue={game.score.visitor}
+                  min={0}
+                  autoComplete="off"
+                  type="number"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.buttonBox}>
+          <div className={styles.dualButtonBox}>
+            <button className={styles.update}>Atualizar</button>
+            <button className={styles.cancel}>Excluir</button>
+          </div>
+
+          <button>Encerrar Jogo</button>
+        </div>
+      </Form>
+    </div>
+  );
 }
