761ab89462edcad340492962c642899acc02a84d
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2022-05-28 15:31:47 -0300

    Improve background video speed

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -4,44 +4,30 @@ import { authContext } from "../contexts/AuthContext";
 import styles from "../styles/components/BackgroundVideo.module.css";
 
 export function BackgroundVideo() {
-  const [pageLoad, setPageLoad] = useState(false);
   const { config } = useContext(configContext);
   const { isFetched, isAuthenticated } = useContext(authContext);
-
-  useEffect(() => {
-    setPageLoad(true);
-  }, []);
 
   return (
     <>
       <div className={styles.overlay} />
-      {config.video && (
-        <video
-          className={styles.background}
-          autoPlay
-          playsInline
-          muted
-          loop
-          poster="/poster.webp"
-        >
-          {pageLoad && isFetched && (
-            <>
-              <source
-                src="https://github.com/Animesports/animesports/blob/main/public/videos/soccer_web.webm?raw=true"
-                type="video/webm"
-              ></source>
-              <source
-                src="https://github.com/Animesports/animesports/blob/main/public/videos/soccer_otz.mp4?raw=true"
-                type="video/mp4"
-              ></source>
-            </>
-          )}
-        </video>
-      )}
 
-      {!config.video && (
-        <img className={styles.default} src="/poster.webp" alt="bf" />
-      )}
+      <video
+        className={styles.background}
+        autoPlay
+        playsInline
+        muted
+        loop
+        poster="/poster.webp"
+      >
+        {isFetched && (config.video || !isAuthenticated) && (
+          <>
+            <source
+              src="https://res.cloudinary.com/hugorodriguesqw/video/upload/v1653762511/animesports/background_video.webm"
+              type="video/webm"
+            ></source>
+          </>
+        )}
+      </video>
     </>
   );
 }
