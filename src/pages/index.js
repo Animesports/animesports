f6ae4b99fca1db442bc425e8d0ad234cdb023057
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2022-06-02 16:40:02 -0300

    Animesports logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Router from "next/router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { HeadComponents } from "../components/HeadComponents";
 import { authContext } from "../contexts/AuthContext";
+
 import styles from "../styles/pages/Home.module.css";
+
 export default function Home() {
   const { isAuthenticated, isFetched } = useContext(authContext);
 
@@ -11,12 +13,23 @@ export default function Home() {
     Router.push("/soccer");
   }
 
+  const [useText, setUserText] = useState(false);
+
   return (
     <>
       <HeadComponents current="/" />
       <div className={styles.container}>
         <div className={styles.content}>
-          <h1 className="appear">Animesports</h1>
+          <h1 className="appear">
+            {!useText && (
+              <img
+                src="/logoExtended.svg"
+                alt="Animesports"
+                onError={() => setUserText(true)}
+              />
+            )}
+            {useText && "Animesports"}
+          </h1>
           <button
             className={[
               "appear",
